566619dd89cf0b5c6968c3993d6a8694cf393309
zkrakus <zkrakus@gmail.com>
2025-05-14 13:54:06 -0400

    feat: cleaned up delete confirmation to auto confirm after 3 seconds to remove a saved location.

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onConfirm();
+
+    }, 1000 * 3);
+
+    return clearTimeout(timer);
+  }, []);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,26 +1,24 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import {  useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
-  const dialog = useRef();
+const Modal = function Modal({ open, children, onClose }) {
+  const dialog = useRef(); 
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+  useEffect(() => {
+    if(open){
+      dialog.current.showModal();
+    }
+    else{
+      dialog.current.close();
+    }
+  },[open])
 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
+    <dialog className="modal" ref={dialog} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById('modal')
   );
-});
+};
 
 export default Modal;
